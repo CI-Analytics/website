ed9a56e570bd98801eecefea83be0f187b2c20a7
BerndCS <larschristianreichardt@gmail.com>
2026-01-28 06:51:13 +0100

    website: render migration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import cloudflare from '@astrojs/cloudflare';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ci-analytics.org',
   base: '/',
   output: 'hybrid',
-  adapter: cloudflare({
-    mode: 'directory'
+  adapter: node({
+    mode: 'standalone'
   }),
   integrations: [react()],
 });
